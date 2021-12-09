cf1334e6dcd3c997bf655acd27918b08561d0fc9
Robert-Rodriguez <robertmasters@hotmail.com>
2021-12-09 17:46:15 -0500

    removed unessasary div

--- pokehuddle/src/components/LoginRegister/Header.js
+++ pokehuddle/src/components/LoginRegister/Header.js
@@ -3,9 +3,7 @@ import React from 'react'
 export default function Header() {
 	return (
 		<header className = "header">
-			<div className = "banner">
 				<img className ="bannerimg" src = {require('../../images/PokeHuddle-Title.png').default} alt = 'pokehuddle' />
-			</div>
 		</header>
 	)
 }
