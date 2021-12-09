d2129350992ff9a832d48239f2b80942329da27a
Robert-Rodriguez <robertmasters@hotmail.com>
2021-12-09 17:21:04 -0500

    finished app component and testing for it

--- pokehuddle/src/App.js
+++ pokehuddle/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div data-testid="app" className="main-container">
+            <div className="Route-container">
+
+            <a target="_blank" className = "link" rel="noreferrer" href="https://github.com/robertmasters/pokehuddle/blob/main/Application_build_plan.md">Github  </a>with roadmap for app
+        </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
