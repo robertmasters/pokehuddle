90050c511db91d4798c25c7f8f343a14b4368082
Robert-Rodriguez <robertmasters@hotmail.com>
2021-12-13 23:58:55 -0500

    added favorite page

---
+++ pokehuddle/src/components/Pages/FavoritePokemon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FavoritePokemon() {
+    return (
+        <div className = "component-container">
+            FavoritePokemon
+        </div>
+    )
+}
