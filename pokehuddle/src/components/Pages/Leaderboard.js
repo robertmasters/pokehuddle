0a07fdb26fdcbdc33ecbed7c493ded14487dc270
Robert-Rodriguez <robertmasters@hotmail.com>
2021-12-14 00:00:27 -0500

    added leaderboard page

---
+++ pokehuddle/src/components/Pages/Leaderboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Leaderboards() {
+    return (
+        <div className = "component-container">
+            Leaderboards
+        </div>
+    )
+}
