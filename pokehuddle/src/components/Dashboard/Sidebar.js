e13ec2f164ed1df024590bad608401cdef805391
Robert-Rodriguez <robertmasters@hotmail.com>
2021-12-13 23:50:53 -0500

    added dashboard routes, still need to make components that it routes to, same with the sidebar links

--- pokehuddle/src/components/Dashboard/Sidebar.js
+++ pokehuddle/src/components/Dashboard/Sidebar.js
@@ -1,25 +1,34 @@
 import React from 'react'
-import {Route, Switch, useRouteMatch } from 'react-router-dom'
-import FavoritePokemon from '../Pages/FavoritePokemon';
-import Home from '../Pages/Home';
-import Leaderboards from '../Pages/Leaderboards';
-import PageNotFound from '../Pages/PageNotFound';
-import PersonalNotes from '../Pages/PersonalNotes';
-import Photos from '../Pages/Photos';
-import Research from '../Pages/Research';
+import {Link, useRouteMatch} from 'react-router-dom';
+import './DashboardStyles/SideBar.css'
 
-export default function DashboardRoutes() {
+function SideBar() {
     const {path} = useRouteMatch()
 
-	return (
-			<Switch>
-				<Route path = {`${path}/home`} component = {Home} />
-				<Route path = {`${path}/research`} component = {Research} />
-				<Route path = {`${path}/favoritepokemon`} component = {FavoritePokemon} />
-				<Route path = {`${path}/leaderboards`} component = {Leaderboards} />
-				<Route path = {`${path}/personalnotes`} component = {PersonalNotes} />
-				<Route path = {`${path}/photos`} component = {Photos} />
-				<Route path = {'*'} component = {PageNotFound} /> 
-			</Switch>
-	);
-};
+    const logout = () => {
+        //also send a request to /api/logout once api is set up
+        // window.localStorage.removeItem('token')
+        console.log("clicked logged out")
+      }
+
+    return (
+        <div className = "nav-bar-container">
+            <div className = "border-line">
+                <div className= "img-wrapper">
+                    <img className ="avatar-img" src = {require('../../images/Avatars/mewtwoAvatar.png').default} alt = 'Avatar' />
+                </div>
+                <div className = "links-container">
+                    <Link className = "link" to ={`${path}/home`}>Home</Link>
+                    <Link className = "link" to ={`${path}/research`}>Research</Link>
+                    <Link className = "link" to ={`${path}/favoritepokemon`}>Favorite Pokemon</Link>
+                    <Link className = "link" to ={`${path}/leaderboards`}>Leaderboards</Link>
+                    <Link className = "link" to ={`${path}/personalnotes`}>Personal Notes</Link>
+                    <Link className = "link" to ={`${path}/photos`}>Photos</Link>
+                    <Link className = "link" onClick={logout} to ='/'>Logout</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SideBar;
