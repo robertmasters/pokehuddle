3106e63f4c0b6a46348a4600404864165249e1cb
Robert-Rodriguez <robertmasters@hotmail.com>
2021-12-13 23:56:56 -0500

    added home component

---
+++ pokehuddle/src/components/Pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Home.css'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome!</h1>
+            <div className = "home-container">
+                <div className = "favorite-notification-container">
+                    <div>
+                        <img className ="favoritestextimg" src = {require('../../images/favoritestext.png').default} alt = 'favorites' />
+                    </div>
+                    <div>
+                        <img className ="notificationstextimg" src = {require('../../images/notificationstext.png').default} alt = 'notifications' />
+                    </div>
+                </div>
+                
+                <div className = "news">
+                    <img className ="newstextimg" src = {require('../../images/newstext.png').default} alt = 'news' />
+                </div>
+            </div>
+        </div>
+    )
+}
