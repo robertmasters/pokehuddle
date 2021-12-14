01fbbc87a87983adb95f5bf41f87f24c83837ed9
Robert-Rodriguez <robertmasters@hotmail.com>
2021-12-14 12:36:38 -0500

    changed directory of dashboard css files, also updated dashboard to follow react router v6 use of route paths, no longer a need for useMatch

--- pokehuddle/src/components/Dashboard/DashboardRoutes.js
+++ pokehuddle/src/components/Dashboard/DashboardRoutes.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import {Route, Switch, useRouteMatch } from 'react-router-dom'
+import {Route, Routes, useMatch } from 'react-router-dom'
 import FavoritePokemon from '../Pages/FavoritePokemon';
 import Home from '../Pages/Home';
-import Leaderboards from '../Pages/Leaderboards';
+import Leaderboard from '../Pages/Leaderboard';
 import PageNotFound from '../Pages/PageNotFound';
 import PersonalNotes from '../Pages/PersonalNotes';
 import Photos from '../Pages/Photos';
 import Research from '../Pages/Research';
 
 export default function DashboardRoutes() {
-    const {path} = useRouteMatch()
+    // const match = useMatch()
 
 	return (
-			<Switch>
-				<Route path = {`${path}/home`} component = {Home} />
-				<Route path = {`${path}/research`} component = {Research} />
-				<Route path = {`${path}/favoritepokemon`} component = {FavoritePokemon} />
-				<Route path = {`${path}/leaderboards`} component = {Leaderboards} />
-				<Route path = {`${path}/personalnotes`} component = {PersonalNotes} />
-				<Route path = {`${path}/photos`} component = {Photos} />
-				<Route path = {'*'} component = {PageNotFound} /> 
-			</Switch>
+			<Routes>
+				<Route path = 'home' element = {<Home />} />
+				<Route path = 'research' element = {<Research />} />
+				<Route path = 'favoritepokemon' element = {<FavoritePokemon />} />
+				<Route path = 'leaderboard' element = {<Leaderboard />} />
+				<Route path = 'personalnotes' element = {<PersonalNotes />} />
+				<Route path = 'photos' element = {<Photos />} />
+				<Route path = {'*'} element = {<PageNotFound />} /> 
+			</Routes>
 	);
 };
