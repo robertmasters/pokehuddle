249681d84cda1d7834be78b52595974ebf4f8b27
Robert-Rodriguez <robertmasters@hotmail.com>
2021-12-14 23:56:14 -0500

    finished unit testing sidebar

---
+++ pokehuddle/src/components/Dashboard/SideBar.test.js
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import SideBar from './Dashboard';
+
+describe('Test Dashboard', () => {
+	test('renders component without crashing', () => {
+		render(
+			<MemoryRouter>
+				<SideBar />
+			</MemoryRouter>
+		)
+	});
+	test('renders avatar', () => {
+		render(
+			<MemoryRouter>
+				<SideBar />
+			</MemoryRouter>
+		)
+		const avatarImg = screen.getByAltText('Avatar')
+		expect(avatarImg).toBeInTheDocument()
+	});
+	test('renders all links', () => {
+		render(
+			<MemoryRouter>
+				<SideBar />
+			</MemoryRouter>
+		)
+		const homeLink = screen.getByText('Home')
+		const researchLink = screen.getByText('Research')
+		const favoritepokemonLink = screen.getByText('Favorite Pokemon')
+		const leaderboardLink = screen.getByText('Leaderboard')
+		const personalnotesLink = screen.getByText('Personal Notes')
+		const photosLink = screen.getByText('Photos')
+		const logoutLink = screen.getByText('Logout')
+
+		expect(homeLink).toBeInTheDocument()
+		expect(researchLink).toBeInTheDocument()
+		expect(favoritepokemonLink).toBeInTheDocument()
+		expect(leaderboardLink).toBeInTheDocument()
+		expect(personalnotesLink).toBeInTheDocument()
+		expect(photosLink).toBeInTheDocument()
+		expect(logoutLink).toBeInTheDocument()
+	});
+})
