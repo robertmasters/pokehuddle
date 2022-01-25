af20c47815d802bc4f5e46e106bba0e5e3b418e3
Robert-Rodriguez <robertmasters@hotmail.com>
2022-01-24 21:25:24 -0500

    added sidebar to the dashboard component

--- pokehuddle/src/components/Dashboard/Dashboard.js
+++ pokehuddle/src/components/Dashboard/Dashboard.js
@@ -1,13 +1,14 @@
 import React from 'react';
-// import SideBar from './SideBar' 
 import './Dashboard.css' 
 import DashboardRoutes from './DashboardRoutes';
+import SideBar from './SideBar';
 
 function Dashboard() {
     return (
             <div data-testid = "dashboard" className= "dash-container">
                 <div className= "center-content">
                     <div className= "sidebar">
+                        <SideBar />
                     </div>
                     <DashboardRoutes />
                 </div>
