d9d555a77cce94130299a70307681c392b5dfd9c
Robert-Rodriguez <robertmasters@hotmail.com>
2022-03-10 20:18:18 -0500

    removed localhost, will add an base url env var later

--- pokehuddle/src/utils/axiosWithAuth.js
+++ pokehuddle/src/utils/axiosWithAuth.js
@@ -5,11 +5,8 @@ export const axiosWithAuth = () => {
     const token = window.localStorage.getItem("pokehuddle-token")
     return axios.create({
         headers: {
-            Authorization: `Bearer ${JSON.parse(token)}`,
+            Authorization: `Bearer ${token}`,
         },
         baseURL: "https://masters-pokehuddlerest.herokuapp.com/"
-        // baseURL: "http://localhost:2019",
-
-        
     })
 }
