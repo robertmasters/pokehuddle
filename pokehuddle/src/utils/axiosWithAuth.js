017b16394acf19de7695419864597b9199a4d491
Robert-Rodriguez <robertmasters@hotmail.com>
2022-03-15 19:43:50 -0400

    Parsed token so that it sends correctly to the backened

--- pokehuddle/src/utils/axiosWithAuth.js
+++ pokehuddle/src/utils/axiosWithAuth.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 //returns new instance of axios with the config object built into it
 export const axiosWithAuth = () => {
-    const token = window.localStorage.getItem("pokehuddle-token")
+    const token = JSON.parse(window.localStorage.getItem("pokehuddle-token"))
     return axios.create({
         headers: {
             Authorization: `Bearer ${token}`,
         },
-        baseURL: "https://masters-pokehuddlerest.herokuapp.com/"
+        baseURL: "https://masters-pokehuddlerest.herokuapp.com"
     })
 }
