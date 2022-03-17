72b97a36ec371f273ba3b1cc3479ab4b1d5c41bc
Robert-Rodriguez <robertmasters@hotmail.com>
2022-03-17 11:34:02 -0400

    corrected image path

--- pokehuddle/src/components/Pages/PageUnderConstruction.js
+++ pokehuddle/src/components/Pages/PageUnderConstruction.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PageUnderConstruction() {
   return (
 	<div> 
-		<img src={require('../../images/Avatars/PageUnderConst.png').default} alt = "page is under construction" />
+		<img src={require('../../images/PageUnderConst.png').default} alt = "page is under construction" />
 	</div> 
   )
 }
