0921e8d7636c74a3dcd665437f188c2e35bace0f
Robert-Rodriguez <robertmasters@hotmail.com>
2022-03-17 11:32:25 -0400

    Created page under construction component

---
+++ pokehuddle/src/components/Pages/PageUnderConstruction.js
@@ -0,0 +1,9 @@
+import React from 'react'
+ 
+export default function PageUnderConstruction() {
+  return (
+	<div> 
+		<img src={require('../../images/Avatars/PageUnderConst.png').default} alt = "page is under construction" />
+	</div> 
+  )
+}
